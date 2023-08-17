d0a5ece945b4ebcd3a28c4ba26b95b6da920cb42
Kartik <kumarkartik147359@gmail.com>
2023-08-17 23:02:43 +0530

    made normal calc screen

--- app/src/main/java/com/example/calculator/Normal.java
+++ app/src/main/java/com/example/calculator/Normal.java
@@ -3,6 +3,11 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Normal extends AppCompatActivity {
 
